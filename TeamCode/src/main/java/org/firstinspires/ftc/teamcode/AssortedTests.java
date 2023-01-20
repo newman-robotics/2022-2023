b9cb2b545c9792c59f5107acfcf5f89bd723ce01
picawawa4000 <114023995+picawawa4000@users.noreply.github.com>
2023-01-20 13:54:26 -0500

    Added AssortedTests
    Took about 10 minutes to write

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AssortedTests.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.RobotController;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.util.Thread;
+
+@TeleOp(name="CameraTest",group="Tests")
+public class CameraTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        AutonomousImageProcessor processor = new AutonomousImageProcessor(WebcamName.class);
+        zone = processor.getSignalSleeveOrientation;
+        telemetry.addData("zone: ",zone);
+        return;
+    }
+}
+
+@TeleOp(name="MotorTest",group="Tests")
+public class MotorTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        DcMotor ul = hardwareMap.get(DcMotor.class, "ltMotor");
+        DcMotor ur  = hardwareMap.get(DcMotor.class, "rtMotor");
+        DcMotor bl = hardwareMap.get(DcMotor.class, "lbMotor");
+		DcMotor br = hardwareMap.get(DcMotor.class, "rbMotor");
+        ul.setPower(0.6);
+        ur.setPower(0.6);
+        bl.setPower(0.6);
+        br.setPower(0.6);
+        Thread.currentThread().sleep(1000);
+        ul.setPower(0);
+        ur.setPower(0);
+        bl.setPower(0);
+        br.setPower(0);
+        return;
+    }
+}
