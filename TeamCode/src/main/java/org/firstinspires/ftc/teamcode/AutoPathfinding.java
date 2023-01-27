3d4a836cb78a08b398a0bb2d1124766a4a055b88
Declan Scott <Datoneguy246@gmail.com>
2023-01-27 09:14:02 -0500

    Start of BFS Pathfinding w/ encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPathfinding.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPathfinding.java
@@ -2,14 +2,19 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+/*
+@author Declan J. Scott
+ */
+
 @Autonomous
 public class AutoPathfinding extends RobotController {
 
+    // (y, x) coordinate system
     private char[][] field = {
-            {'0','0','0','4'},
-            {'3','0','0','0'},
-            {'0','0','2','0'},
-            {'0','0','0','1'},
+            {'_','_','_','_'},
+            {'_','_','_','_'},
+            {'_','_','_','_'},
+            {'_','_','_','_'},
     };
     private double distanceBetweenPoints = 1; // in meters
 
