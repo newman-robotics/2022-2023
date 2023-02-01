e0593f19c205a483508add84753532f593d43ddb
picawawa4000 <114023995+picawawa4000@users.noreply.github.com>
2023-02-01 16:27:08 -0500

    Update CameraTest.java
    most likely still won't work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTest.java
@@ -3,13 +3,15 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.AutonomousDriver;
+
 import org.opencv.videoio.VideoCapture;
 
 @TeleOp(name="CameraTest",group="Tests")
 public class CameraTest extends LinearOpMode {
     @Override
     public void runOpMode() {
-        AutonomousImageProcessor processor = new AutonomousImageProcessor(new VideoCapture(0));
+        AutonomousDriver.AutonomousImageProcessor processor = new AutonomousDriver.AutonomousImageProcessor(new VideoCapture(0));
         Byte zone = processor.getSignalSleeveOrientation();
         telemetry.addData("zone: ",zone);
         return;
